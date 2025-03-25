f743ce595d1a53aeab5354719dc19d06318100b8
Aniruddha Dev Nath <113041453+ARNOB663@users.noreply.github.com>
2025-03-25 10:48:16 +0600

    Create app.js

---
+++ Six JavaScript Fundamentals for React/app.js
@@ -0,0 +1,26 @@
+//variable 
+// let or const insted of var 
+//let and const 
+let name="arnob"
+name  = "arnob dev nath"
+//we can change the value of a variable if we use let 
+const Country_name="BD"
+//we can not change the value of a variable if we use const 
+
+const num=40;
+
+if(num>=20 || num<=30)
+{
+    console.log("yes")
+}
+else
+{
+    console.log("Nooo")
+}
+
+const friends=["HERO","JKR","SRK"]
+console.log(friends)//prints the array
+console.log(friends[1])//prints the value of 1st index
+console.log(friends.length)//prints the length of the array
+console.log(friends)
+
