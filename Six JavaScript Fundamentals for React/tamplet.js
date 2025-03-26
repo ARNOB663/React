fa9180ec1285519664224844cc5565044f9e60c7
Aniruddha Dev Nath <113041453+ARNOB663@users.noreply.github.com>
2025-03-26 11:25:51 +0600

    Update tamplet.js

--- Six JavaScript Fundamentals for React/tamplet.js
+++ Six JavaScript Fundamentals for React/tamplet.js
@@ -1,1 +1,13 @@
-``
+const fname="Arnob"
+const lname="Dev nath"
+console.log(fname+lname)
+
+console.log(`i am ${fname} ${lname}`)
+
+
+function sum(){
+
+    console.log(arguments)
+}
+const sum1=(a,b)=> a+b
+
