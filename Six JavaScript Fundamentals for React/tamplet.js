7c065781767e95b817a91429752cb5b2e52fed67
Aniruddha Dev Nath <113041453+ARNOB663@users.noreply.github.com>
2025-03-26 11:55:10 +0600

    map

--- Six JavaScript Fundamentals for React/tamplet.js
+++ Six JavaScript Fundamentals for React/tamplet.js
@@ -1,13 +1,50 @@
-const fname="Arnob"
-const lname="Dev nath"
-console.log(fname+lname)
+// Declaring two string variables
+const fname = "Arnob";
+const lname = "Dev nath";
 
-console.log(`i am ${fname} ${lname}`)
+// Concatenating strings using the + operator
+console.log(fname + lname);
 
+// Using template literals to format and log a string
+console.log(`i am ${fname} ${lname}`);
 
-function sum(){
-
-    console.log(arguments)
+// Function declaration using the `function` keyword
+// This function logs the `arguments` object, which contains all arguments passed to the function
+function sum() {
+    console.log(arguments);
 }
-const sum1=(a,b)=> a+b
+
+// Arrow function that takes two arguments and returns their sum
+const sum1 = (a, b) => a + b;
+
+// Arrow function using the rest parameter to collect all arguments into an array
+// Logs the array of arguments passed to the function
+const sum2 = (...rest) => {
+    console.log(rest);
+};
+
+// Calling `sum2` with multiple arguments, which will be logged as an array
+sum2(1, 2, 3, 4, 5);
+
+// Arrow function that takes three arguments and returns their sum
+const sum3 = (a, b, c) => a + b + c;
+
+// Calling `sum3` with three arguments and storing the result in a variable
+const result = sum3(1, 2, 3);
+
+// Logging the result of `sum3` with three arguments
+console.log(sum3(1, 2, 3));
+
+// Calling `sum3` with more arguments than it expects
+// Only the first three arguments will be used, as the function is defined to take three parameters
+console.log(sum3(1, 2, 3, 4, 5));
+
+const number = [1,2,3,4,5,6]
+
+console.log(...number ) 
+//... spred oparator
+const newNumber = [...number , 10 , 11 , 1000]
+
+console.log(...newNumber)
+
 
---
+++ array-methods.js
@@ -0,0 +1,16 @@
+// array of object
+const products = [
+{ name:"xiaomi",brand:"xiaomi",price:3000,color:"Black"},
+{ name:"samsumg",brand:"samsumg",price:3000,color:"Black"},
+{ name:"apple1",brand:"apple",price:16000,color:"Black"},
+{ name:"m2",brand:"xiaomi",price:3000,color:"Black"},
+{ name:"oneplus10r",brand:"oneplus",price:3000,color:"Black"},
+]
+// returns the all the product price in an array using map
+const result = products.map(product=>product.price)
+console.log(...result)
+// returns the all the product name in an array using map
+const names = products.map(product=>product.name)
+console.log(names)
+const brand = products.map(brands=>brands.brand)
+console.log(...brand)
