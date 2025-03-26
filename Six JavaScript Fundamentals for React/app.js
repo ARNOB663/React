35a9174bc43dcb7b552ea7bee9191b3df271c71e
Aniruddha Dev Nath <113041453+ARNOB663@users.noreply.github.com>
2025-03-26 11:21:09 +0600

    u

--- Six JavaScript Fundamentals for React/app.js
+++ Six JavaScript Fundamentals for React/app.js
@@ -27,4 +27,38 @@ friends.push("US")//insterts a new string in the array
 console.log(friends)
 friends.unshift("BD")//puts bd in the front
 console.log(friends)
+friends.shift()//remove value of an array from the front
+console.log(friends)
+
+console.log(friends.slice(2,4))//return from index from 2 to 4
+
+//normal for loop
+
+for(let i=0;i<friends.length;i++)
+{
+    const element = friends[i];
+    console.log(element)
+}
+//function 
+function sum(a,b){ 
+ console.log(arguments)//shows the number of argumnents and the arguments
+ console.log(...arguments)//prints the arguments 
+
+    console.log(a+b)//returns the sun of a and b
+    return a+b
+}
+sum(2,3)//funtion call
+const output=sum(2,3)
+//object 
+const person = {
+    name:"arnob",
+    friends:friends,
+    age:10,
+}
+console.log(person)//print the object
+console.log(person.name)//prints the name from object 
+console.log(person.friends[1])//prints the array index fromn the array 
+person.name="Daddy"
+console.log(person)
+
 
