f9888c6770c64f5a92c4fe6174229096010020d7
Aniruddha Dev Nath <113041453+ARNOB663@users.noreply.github.com>
2025-03-26 12:41:16 +0600

    Create destructuring.js

---
+++ Six JavaScript Fundamentals for React/destructuring.js
@@ -0,0 +1,12 @@
+const friends=["Kuddus","Akkas","bakkas"]
+const [element1,element2,element3] =friends;
+console.log(element1,element2,element3)
+
+const person= {
+ name:"kuddus",
+ age:"24",
+ friends:["Akkas","Bakkas"],
+ country:"BD"
+}
+
+console.log(person.coutry)
