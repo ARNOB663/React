8f576dd2a24162c464fbd2db5e661ea32b658fb1
Aniruddha Dev Nath <113041453+ARNOB663@users.noreply.github.com>
2025-04-03 02:49:08 +0600

    add the visited country in a array

--- react-world-on-the-go/src/components/Countries/Countries.jsx
+++ react-world-on-the-go/src/components/Countries/Countries.jsx
@@ -2,18 +2,33 @@ import React from 'react';
 import { use } from 'react';
 import Country from '../Country/Country';
 import './Countries.css';
+import { useState } from 'react';
 const Countries = ({countriesPromise}) => {
+    const [visitedCounrtries,setVisitedCountries] = useState([])
    
-
     const countries=use(countriesPromise);
-    
 
+    const handleVisitedCountries = (country) =>{
+        console.log('country vsited clicked to be added',country)
+        const newVisitedCountries = [...visitedCounrtries,country]
+        setVisitedCountries(newVisitedCountries);
+       // visitedCounrty.push(country)
+    }
+    
     return (
         <div  >
             <h1>Traveling Countries: {countries.length}</h1>
+            <h3>Traveled so far: {visitedCounrtries.length}</h3>
+            <ol>
+                {
+                    visitedCounrtries.map(country => <li>{country.name.common}</li>)
+                }
+            </ol>
             <div className='countries'>
             {
-                countries.map(country => <Country key={country.cca3} country={country} ></Country>)
+                countries.map(country => <Country 
+                handleVisitedCountries={handleVisitedCountries}
+                key={country.cca3} country={country} ></Country>)
             }
             </div>
           
--- react-world-on-the-go/src/components/Country/Country.jsx
+++ react-world-on-the-go/src/components/Country/Country.jsx
@@ -2,22 +2,33 @@ import React from 'react';
 import './Country.css'
 import { useState } from 'react';
 
-const Country = ({country}) => {
+const Country = ({country,handleVisitedCountries}) => {
 
 
     const [visited,setVisited] =  useState(false)
 const handleVisited = () =>{
-    setVisited(true)
-}
 
-    console.log(country)
+    //   if(visited ===  true)
+    //   {
+    //     setVisited(false)
+    //   }
+    //   else{
+    // setVisited(true)
+    //   }
+
+     setVisited(!visited)// if the value is true it turn the value to false if false tuern this into true
+     handleVisitedCountries(country)
+
+}
     return (
-        <div className='country'>
+        <div className={`country ${visited && 'country-visited'}`}>
             <h3>Name: {country.name.common} </h3>
             <img src={country.flags.png}></img>
             <p>Independent: {country.independent ? 'Free': 'Not Free'}</p>
             <p>Population: {country.population}</p>
-         <button onClick={handleVisited} >{
+         <button  
+         className={visited? 'btn-visited':'btn-not-visited'} 
+         onClick={handleVisited} >{
             visited? "Visited" : "Not Visited"
          }</button>
         </div>
