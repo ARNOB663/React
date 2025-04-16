f100c70eee91aafcf54d1333dad42aed187cbd73
Aniruddha Dev Nath <113041453+ARNOB663@users.noreply.github.com>
2025-04-16 21:33:42 +0600

    axios and graph implimentation

--- tailwind-react/src/App.jsx
+++ tailwind-react/src/App.jsx
@@ -3,8 +3,13 @@ import './App.css'
 import DaisyNav from './components/DaisyNav/DaisyNav'
 import NavBar from './components/NavBar/NavBar'
 import PricingOptions from './PricingOptions/PricingOptions'
+import ResultsChart from './components/ResultsChart/ResultsChart'
+import axios from 'axios'
+import MarksChart from './components/MarksChart/MarksChart'
 
 const pricingPromise = fetch('pricingData.json').then(res => res.json());
+
+const marksPromise = axios.get('marksData.json')
 
 
 function App() {
@@ -18,9 +23,13 @@ function App() {
         </header>   
         <main>
         <Suspense fallback={<span className="loading loading-spinner loading-lg"></span>}>
-
           <PricingOptions pricingPromise={pricingPromise} ></PricingOptions>
+        </Suspense>
 
+        <ResultsChart></ResultsChart>
+
+        <Suspense fallback={<span className="loading loading-spinner loading-lg"></span>}>
+          <MarksChart marksPromise={marksPromise} ></MarksChart>
         </Suspense>
           
         </main>
---
+++ tailwind-react/src/components/MarksChart/MarksChart.jsx
@@ -0,0 +1,39 @@
+import React, { use } from 'react';
+import { Bar, BarChart, XAxis, YAxis } from 'recharts';
+
+const MarksChart = ({marksPromise}) => {
+
+    const markDataRes = use(marksPromise)
+    const marksData = markDataRes.data;
+    
+    //data processing for the chart
+    const marksChartData =  marksData.map(studentData => {
+
+        const student = {
+            id:studentData.student_id,
+            name:studentData.name,
+            phy: studentData.physics,
+            chem: studentData.chemistry,
+            math: studentData.math
+        }
+        const avg = (student.phy + student.chem + student.math)/3
+        student.avg = avg;
+        return student
+    })
+ 
+    console.log(marksChartData)
+     
+    return (
+        <div>
+            <BarChart width={500} height={300} data={marksChartData}>
+             <XAxis dataKey="name"></XAxis>
+             <YAxis ></YAxis>
+            <Bar dataKey="avg" fill='#1E90FF' ></Bar>
+                
+            </BarChart>
+
+        </div>
+    );
+};
+
+export default MarksChart;
