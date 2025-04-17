fedb4f5f884170b6ca7c81522405cc5ffee5e845
Aniruddha Dev Nath <113041453+ARNOB663@users.noreply.github.com>
2025-04-17 10:50:21 +0600

    project setup + Routing setup

--- intro-to-react-router/src/main.jsx
+++ intro-to-react-router/src/main.jsx
@@ -2,24 +2,42 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { createBrowserRouter, RouterProvider } from 'react-router';
+import Root from './components/Root/Root.jsx';
+import Home from './components/Home/Home.jsx';
+import Mobiles from './components/Mobiles/Mobiles.jsx';
+import Laptops from './Laptops/Laptops.jsx';
 
 const router = createBrowserRouter([
   {
-    path: "/",
-    element: <App />,
-    
+    path: '/',
+   Component: Root,
+   children:[
+     {index: true, Component: Home},
+     {path:'mobiles',Component: Mobiles},
+     {path:'laptops',Component:Laptops,}
+     
+     
+   ]
   },
-]);
-
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router";
+  {
+    path: 'about',
+    element: <div>Aboute Me Here</div>
+  },
+  {
+    path: 'blogs',
+    element: <div>All my blogs are here</div>
+  },{
+     path: 'app',
+     Component: App
+     //or can be done by element: <App></App>
+    }
+])
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <RouterProvider router={router}></RouterProvider>
+       <RouterProvider router={router} ></RouterProvider>
   </StrictMode>,
 )
 
