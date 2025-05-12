cab0e0168d20ca697ecf90aa9149a46a1d3a6e5e
Aniruddha Dev Nath <113041453+ARNOB663@users.noreply.github.com>
2025-05-12 23:05:50 +0600

    Update Header.jsx

--- intro-to-react-router/src/components/Header/Header.jsx
+++ intro-to-react-router/src/components/Header/Header.jsx
@@ -1,10 +1,26 @@
 import React from 'react';
+import App from '../../../../explor-code-react/src/App';
+import Mobiles from '../Mobiles/Mobiles';
+import { Link  } from 'react-router';
+import Laptops from '../../Laptops/Laptops';
+import './Header.css';
 
 
 const Header = () => {
     return (
         <div>
             <h3>This is header</h3>
+            <nav>
+                {/* <a href='/'>Home</a>
+                <a href='/mobiles'>Mobiles</a>
+                <a href='/laptops'>laptops</a>
+                <a href=''></a> */}
+
+            <Link  to='/'>Home</Link>
+            <Link  to='/mobiles'>Mobiles</Link>
+            <Link  to='/laptops'>Laptops</Link>
+            <Link  to='/'>Home</Link>
+            </nav>
         </div>
     );
 };
