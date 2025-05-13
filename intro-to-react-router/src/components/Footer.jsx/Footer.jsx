6082d7a1f6e4c603b51b7d3e85ee9d4a633c5e63
Aniruddha Dev Nath <113041453+ARNOB663@users.noreply.github.com>
2025-05-13 15:48:27 +0600

    update

---
+++ intro-to-react-router/src/components/Footer.jsx/Footer.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div>
+            <p>
+                <small>Thank you for visiting our website</small>
+                <a href='/'>terms</a>
+                <a href='/'>privacy</a>
+                <a href='/'>policy</a>
+                <a href='/'>contact</a>
+                <a href='/'>about</a>
+                <a href='/'>help</a>
+                <a href='/'>support</a>
+            </p>
+        </div>
+    );
+};
+
+export default Footer;
--- intro-to-react-router/src/components/Root/Root.jsx
+++ intro-to-react-router/src/components/Root/Root.jsx
@@ -1,12 +1,24 @@
 import React from 'react';
 import Header from '../Header/Header';
 import { Outlet } from 'react-router';
+import Footer from '../Footer.jsx/Footer';
 
 const Root = () => {
     return (
         <div>
             <Header></Header>
-            <Outlet></Outlet>
+            //
+
+            <div>
+                
+                <aside>
+
+
+                </aside>
+                <Outlet></Outlet>
+
+            </div>
+            <Footer></Footer>
         </div>
     );
 };
